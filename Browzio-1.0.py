f56edc43e5b7b564002621f32dce97dd390e7195
Atharva-kelkar05 <97673859+Atharva-kelkar05@users.noreply.github.com>
2022-03-19 00:21:28 +0530

    Add files via upload

---
+++ Browzio-1.0.py
@@ -0,0 +1,67 @@
+import sys
+import webbrowser
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
+import Browzio.Browzio_2 as Browzio_2
+sys.exec_prefix(Browzio_2)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('https://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        #Navigation Bar
+
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+        back_Btn = QAction('Back', self)
+        back_Btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_Btn)
+        
+        #Forward Button
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+        
+        #Reload Button
+        
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        #Home Button
+
+        home_btn= QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        #Address Bar
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+        #Stop searching button
+        stop_btn = QAction('Stop', self)
+        stop_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(stop_btn)
+        
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://google.com'))
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Browser')
+window = MainWindow()
+app.exec_()
+
